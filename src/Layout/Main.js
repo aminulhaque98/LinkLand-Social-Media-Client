889b9d9818f7d4a52a30c888fec152f9e36f2dca
aminulhaque98 <aminulhaque419@gmail.com>
2023-02-27 00:33:40 +0530

    set layout and drawer

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Login from '../Pages/Login/Login';
+import { Link, Outlet } from 'react-router-dom';
 import Footer from '../Pages/Shared/Footer/Footer';
 import Navbar from '../Pages/Shared/Navbar/Navbar';
 
@@ -8,15 +7,23 @@ const Main = () => {
     return (
         <div>
             <Navbar></Navbar>
-            <div class="flex gap-4">
-                <div class="flex-auto w-32">
-                    01
-                </div>
-                <div class="flex-auto w-64 ">
+            <div className="drawer drawer-mobile">
+                <input id="right-drawer" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content">
                     <Outlet></Outlet>
+
                 </div>
-                <div class="flex-auto w-32 ">
-                    03
+                <div className="drawer-side">
+                    <label htmlFor="right-drawer" className="drawer-overlay"></label>
+                    <ul className="menu g-5 p-4 w-80 bg-base-100 text-base-content">
+
+
+                        <li><Link to='/'>All Friends</Link></li>
+
+
+
+                    </ul>
+
                 </div>
             </div>
             <Footer></Footer>
